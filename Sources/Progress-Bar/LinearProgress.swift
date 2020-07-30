35dd5ffb369fa522519c819170e171d0a76dbbea
Ameddah Achraf <fm_ameddah@esi.dz>
2020-07-30 16:11:14 +0100

    [Add] Linear Progress bar

---
+++ Sources/Progress-Bar/LinearProgress.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by BqNqNNN on 7/30/20.
+//
+
+import SwiftUI
+
+public struct LinearProgress: View {
+    var percentage: CGFloat
+    var foregroundColor : LinearGradient
+    var backgroundColor : Color
+    
+    public init(percentage: CGFloat, backgroundColor : Color, foregroundColor: LinearGradient) {
+        self.percentage = percentage
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+    }
+    
+    public var body: some View {
+        ZStack{
+            GeometryReader { geometry in
+                Rectangle().frame(width: min(geometry.size.width, geometry.size.width) , height: geometry.size.height)
+                    .foregroundColor(backgroundColor)
+                    .cornerRadius(geometry.size.width/7)
+                    .opacity(0.5)
+                    .shadow(radius: 1)
+                
+                
+                foregroundColor
+                    .mask(Rectangle()
+                            .cornerRadius(geometry.size.width/7)
+                            .animation(.linear))
+                    .frame(width: min(geometry.size.width*percentage, geometry.size.width) , height: geometry.size.height)
+            }
+        }
+    }
+}
